81c6567e5af66de7442478535c5bd0d430e35d07
sadraq-k <sadrakasaeii@gmail.com>
2024-10-29 22:29:30 +0330

    server for 2client

--- server.cpp
+++ server.cpp
@@ -5,107 +5,70 @@ using namespace std;
 using namespace boost::asio;
 using boost::asio::ip::tcp;
 
-
-
-
 // تابعی برای راه‌اندازی سرور
-auto start_server = [](io_context& io, const std::string& ip, int port) -> tcp::acceptor
-{
+auto start_server = [](io_context& io, const std::string& ip, int port) -> tcp::acceptor {
     tcp::endpoint endpoint(boost::asio::ip::address::from_string(ip), port);
     tcp::acceptor acceptor(io, endpoint);
     acceptor.listen();
     return acceptor;
 };
 
-
-
-
 // تابعی برای دریافت داده از کلاینت
-auto receive_data = [](tcp::socket& socket) -> std::string 
-{
+auto receive_data = [](tcp::socket& socket) -> std::string {
     std::string buffer(1024, '\0');
     std::size_t len = socket.read_some(boost::asio::buffer(&buffer[0], buffer.size()));
     buffer.resize(len);
     return buffer;
 };
 
-
-
 // تابعی برای ارسال پاسخ به کلاینت
-auto send_response = [](tcp::socket& socket, const std::string& response)
-{
+auto send_response = [](tcp::socket& socket, const std::string& response) {
     boost::asio::write(socket, boost::asio::buffer(response));
 };
 
-
-
 // تابع اصلی سرور
-void run_server(const std::string& ip, int port) 
-{
+void run_server(const std::string& ip, int port) {
     io_context io;
-    int i = 0;
-    auto acceptor1 = start_server(io, ip, port);
-     auto acceptor2 = start_server(io, ip, port);
-    //برای قبول درخواست های مداوم کلاینت
-    while (true) 
-    {
-        tcp::socket socket1(io);
-        acceptor1.accept(socket1);
+    auto acceptor = start_server(io, ip, port);
 
-        tcp::socket socket2(io);
-        acceptor2.accept(socket2);
+    // اتصال به دو کلاینت و ذخیره سوکت‌ها
+    tcp::socket client1(io);
+    tcp::socket client2(io);
 
-        try {
-            //برای مداوم در ارتباط بودن با کلاینت
-            while (true) 
-            { // ﺢﻠﻘﻫ ﻥﺎﻤﺣﺩﻭﺩ ﺏﺭﺎﯾ ﺩﺮﯾﺎﻔﺗ ﻭ ﺍﺮﺳﺎﻟ ﺩﺍﺪﻫ
-                auto data1 = receive_data(socket1);
-                auto data2 = receive_data(socket2);
+    cout << "Waiting for Client 1 to connect..." << endl;
+    acceptor.accept(client1);
+    cout << "Client 1 connected." << endl;
 
-                if (data1.empty()) break; // ﺩﺭ ﺹﻭﺮﺗ ﺩﺮﯾﺎﻔﺗ ﺩﺍﺪﻫ ﺥﺎﻠﯾ، ﺢﻠﻘﻫ ﺭﺍ ﺐﺸﮑﻨﯾﺩ
-                if (data2.empty()) break;
+    cout << "Waiting for Client 2 to connect..." << endl;
+    acceptor.accept(client2);
+    cout << "Client 2 connected." << endl;
 
-
-
-                if (data1 == "the end")
-                {
-                    cout<<"\n nice chat bye ;)\n ";
-                    exit(0);
+    try {
+        while (true) {
+            // دریافت پیام از کلاینت اول و ارسال به کلاینت دوم
+            if (client1.available()) {
+                std::string data1 = receive_data(client1);
+                if (!data1.empty()) {
+                    cout << "Client 1: " << data1 << endl;
+                    send_response(client2, "Client 1: " + data1);
                 }
-                if (data2 == "the end")
-                {
-                    cout<<"\n nice chat bye ;)\n ";
-                    exit(0);
-                }
-
-
-                
-                cout << "Received from client1" << data1.size() << " bytes: " << data1 << endl;
-                cout << "Received from client2" << data2.size() << " bytes: " << data2 << endl;
-
-                cout << "Anything else for client?\n";
-
-                string willsend1;
-                string willsend2;
-
-                willsend2 = data1;
-                willsend1 = data2;
-               // getline(cin, willsend1);
-               // getline(cin, willsend2);
-
-                send_response(socket1, willsend1);
-                send_response(socket1, willsend2);
             }
-        } catch (const std::exception& e) 
-        {
-            cerr << "Error: " << e.what() << endl;
-        }
-    }
 
+            // دریافت پیام از کلاینت دوم و ارسال به کلاینت اول
+            if (client2.available()) {
+                std::string data2 = receive_data(client2);
+                if (!data2.empty()) {
+                    cout << "Client 2: " << data2 << endl;
+                    send_response(client1, "Client 2: " + data2);
+                }
+            }
+        }
+    } catch (const std::exception& e) {
+        cerr << "Error: " << e.what() << endl;
+    }
 }
 
-int main() 
-{
+int main() {
     run_server("192.168.57.10", 1403);
     return 0;
 }
