49a17fa7e9384d67593801095bef01a32842e72f
Foad <Foadgolabi@gmail.com>
2024-11-18 00:44:14 +0330

    Waiting on a socket simultaneously to read data for both clients to solve the problem of receiving data from each client simultaneously

--- server.cpp
+++ server.cpp
@@ -2,110 +2,99 @@
 #include <boost/asio.hpp>
 #include <queue>
 #include <unordered_map>
+#include <memory>
 
 using namespace std;
 using namespace boost::asio;
 using boost::asio::ip::tcp;
 
-// تابعی برای راه‌اندازی سرور
-auto start_server = [](io_context& io, const std::string& ip, int port) -> tcp::acceptor {
-    tcp::endpoint endpoint(boost::asio::ip::address::from_string(ip), port);
-    tcp::acceptor acceptor(io, endpoint);
-    acceptor.listen();
-    return acceptor;
-};
+class ChatServer {
+    io_context& io_;
+    tcp::acceptor acceptor_;
+    std::shared_ptr<tcp::socket> client1_;
+    std::shared_ptr<tcp::socket> client2_;
+    queue<string> messages_for_client1_;
+    queue<string> messages_for_client2_;
 
-// تابعی برای دریافت داده از کلاینت
-auto receive_data = [](tcp::socket& socket) -> std::string {
-    std::string buffer(1024, '\0');
-    std::size_t len = socket.read_some(boost::asio::buffer(&buffer[0], buffer.size()));
-    buffer.resize(len);
-    return buffer;
-};
+    void async_read_from_client(std::shared_ptr<tcp::socket> client, queue<string>& message_queue, const string& client_name, std::shared_ptr<tcp::socket> target_client) {
+        auto buffer = make_shared<std::vector<char>>(1024);
+        client->async_read_some(boost::asio::buffer(*buffer), 
+            [this, buffer, client, &message_queue, client_name, target_client](const boost::system::error_code& ec, size_t bytes_transferred) {
+                if (!ec) {
+                    string message(buffer->data(), bytes_transferred);
+                    cout << client_name << ": " << message << endl;
 
-// تابعی برای ارسال پاسخ به کلاینت
-auto send_response = [](tcp::socket& socket, const std::string& response) {
-    boost::asio::write(socket, boost::asio::buffer(response));
-};
-
-// تابع اصلی سرور
-void run_server(const std::string& ip, int port) {
-    io_context io;
-    auto acceptor = start_server(io, ip, port);
-
-    // اتصال به دو کلاینت و ذخیره سوکت‌ها
-    tcp::socket client1(io);
-    tcp::socket client2(io);
-    bool client1_connected = false, client2_connected = false;
-
-    queue<string> messages_for_client1;
-    queue<string> messages_for_client2;
-
-    // انتظار برای اتصال کلاینت‌ها
-    cout << "Waiting for Client 1 to connect..." << endl;
-    acceptor.async_accept(client1, [&](const boost::system::error_code& error) {
-        if (!error) {
-            client1_connected = true;
-            cout << "Client 1 connected." << endl;
-        }
-    });
-
-    cout << "Waiting for Client 2 to connect..." << endl;
-    acceptor.async_accept(client2, [&](const boost::system::error_code& error) {
-        if (!error) {
-            client2_connected = true;
-            cout << "Client 2 connected." << endl;
-        }
-    });
-
-    io.run();
-
-    try {
-        while (true) {
-            // دریافت پیام از کلاینت اول و ارسال به کلاینت دوم یا ذخیره‌سازی
-            if (client1_connected && client1.available()) {
-                std::string data1 = receive_data(client1);
-                if (!data1.empty()) {
-                    cout << "Client 1: " << data1 << endl;
-                    if (client2_connected) {
-                        send_response(client2, "Client 1: " + data1);
+                    if (target_client && target_client->is_open()) {
+                        async_write_to_client(target_client, client_name + ": " + message);
                     } else {
-                        messages_for_client2.push("Client 1: " + data1);
+                        message_queue.push(client_name + ": " + message);
                     }
+
+                    async_read_from_client(client, message_queue, client_name, target_client);
+                } else {
+                    cout << client_name << " disconnected: " << ec.message() << endl;
                 }
-            }
-
-            // دریافت پیام از کلاینت دوم و ارسال به کلاینت اول یا ذخیره‌سازی
-            if (client2_connected && client2.available()) {
-                std::string data2 = receive_data(client2);
-                if (!data2.empty()) {
-                    cout << "Client 2: " << data2 << endl;
-                    if (client1_connected) {
-                        send_response(client1, "Client 2: " + data2);
-                    } else {
-                        messages_for_client1.push("Client 2: " + data2);
-                    }
-                }
-            }
-
-            // ارسال پیام‌های ذخیره‌شده به کلاینت اول
-            if (client1_connected && !messages_for_client1.empty()) {
-                send_response(client1, messages_for_client1.front());
-                messages_for_client1.pop();
-            }
-
-            // ارسال پیام‌های ذخیره‌شده به کلاینت دوم
-            if (client2_connected && !messages_for_client2.empty()) {
-                send_response(client2, messages_for_client2.front());
-                messages_for_client2.pop();
-            }
-        }
-    } catch (const std::exception& e) {
-        cerr << "Error: " << e.what() << endl;
+            });
     }
-}
+
+    void async_write_to_client(std::shared_ptr<tcp::socket> client, const string& message) {
+        auto buffer = make_shared<string>(message);
+        async_write(*client, boost::asio::buffer(*buffer), 
+            [buffer](const boost::system::error_code& ec, size_t) {
+                if (ec) {
+                    cerr << "Error sending message: " << ec.message() << endl;
+                }
+            });
+    }
+
+    void send_pending_messages(std::shared_ptr<tcp::socket> client, queue<string>& message_queue) {
+        while (!message_queue.empty() && client->is_open()) {
+            async_write_to_client(client, message_queue.front());
+            message_queue.pop();
+        }
+    }
+
+public:
+    ChatServer(io_context& io, const string& ip, int port) 
+        : io_(io), acceptor_(io, tcp::endpoint(boost::asio::ip::address::from_string(ip), port)) {}
+
+    void run() {
+        client1_ = make_shared<tcp::socket>(io_);
+        client2_ = make_shared<tcp::socket>(io_);
+
+        cout << "Waiting for Client 1 to connect..." << endl;
+        acceptor_.async_accept(*client1_, [this](const boost::system::error_code& ec) {
+            if (!ec) {
+                cout << "Client 1 connected." << endl;
+                async_read_from_client(client1_, messages_for_client1_, "Client 1", client2_);
+                send_pending_messages(client1_, messages_for_client1_);
+            } else {
+                cerr << "Error accepting Client 1: " << ec.message() << endl;
+            }
+        });
+
+        cout << "Waiting for Client 2 to connect..." << endl;
+        acceptor_.async_accept(*client2_, [this](const boost::system::error_code& ec) {
+            if (!ec) {
+                cout << "Client 2 connected." << endl;
+                async_read_from_client(client2_, messages_for_client2_, "Client 2", client1_);
+                send_pending_messages(client2_, messages_for_client2_);
+            } else {
+                cerr << "Error accepting Client 2: " << ec.message() << endl;
+            }
+        });
+
+        io_.run();
+    }
+};
 
 int main() {
-    run_server("192.168.57.10", 1403);
+    try {
+        io_context io;
+        ChatServer server(io, "192.168.57.10", 1403);
+        server.run();
+    } catch (const std::exception& e) {
+        cerr << "Server error: " << e.what() << endl;
+    }
     return 0;
 }
