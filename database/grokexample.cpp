295e551c3c1752b49d54fc5671966bb0a935ae33
sadraq-k <sadrakasaeii@gmail.com>
2025-03-09 16:16:17 +0330

    now we have managesystem for sqlite 1.manage databace. 2.insert data to databace.

--- database/grokexample.cpp
+++ database/grokexample.cpp
@@ -2,44 +2,20 @@
 // Created by sadraq on 3/8/25.
 //
 #include <iostream>
-#include <sqlite3.h>
+#include <sstream>
 
 using namespace std;
 
-int main() {
-    sqlite3* db;
-    char* errMsg = 0;
-    int rc;
+int main()
+{
+    string name = "sadra" , username = "sadraq" , how_they_find_us = "so hard";
+    stringstream query;
+    query<<"INSERT INTO uers(name,username,how_they_find_us) VALUES('"<<name<<
+         "' , '"<<username<<"' , '" <<how_they_find_us<<"');";
 
-    // باز کردن دیتابیس
-    rc = sqlite3_open("informations.db", &db);
-    if (rc) {
-        cerr << "نمی‌تونم دیتابیس رو باز کنم: " << sqlite3_errmsg(db) << endl;
-        return 1;
-    }
+    string finalquery;
 
-    // ساخت یه جدول
-    const char* sql = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);";
-    rc = sqlite3_exec(db, sql, 0, 0, &errMsg);
-    if (rc != SQLITE_OK) {
-        cerr << "خطا تو ساخت جدول: " << errMsg << endl;
-        sqlite3_free(errMsg);
-    } else {
-        cout << "جدول با موفقیت ساخته شد!" << endl;
-    }
-
-    // وارد کردن داده
-    sql = "INSERT INTO users (name, age) VALUES ('Ali', 30);";
-    rc = sqlite3_exec(db, sql, 0, 0, &errMsg);
-    if (rc != SQLITE_OK)
-    {
-        cerr << "خطا تو وارد کردن داده: " << errMsg << endl;
-        sqlite3_free(errMsg);
-    } else {
-        cout << "داده با موفقیت وارد شد!" << endl;
-    }
-
-    // بستن دیتابیس
-    sqlite3_close(db);
+    finalquery = query.str();
+    cout<<finalquery;
     return 0;
 }
--- database/informations.cpp
+++ database/informations.cpp
@@ -3,16 +3,124 @@
 //
 #include <iostream>
 #include <sqlite3.h>
-
-
+#include <sstream>
 
 using namespace std;
+
+int insert_data(sqlite3 *db ,string name,string username,string how_they_find_us);
 
 int main()
 {
     //creat a databace
     sqlite3* db;
+
+    //we creat one of this and to rest of code we gona use this and
+    //because this we gana findout it work well or not
+    int return_code = 0;
+
+    //I don't know what the fuck is this.
     sqlite3_stmt* stmt;
-    sqlite3_open("informations.db", &db);
+
+    //when we want to exe a query or some of that we put them in this string.
+    string sql_query ;
+    char * errmsg;
+
+
+    return_code = sqlite3_open("users.db", &db);
+
+    //SQLITE_OK : it gona be findout it's ok or not.
+    if (return_code != SQLITE_OK)
+    {
+        //this option for sqlite "sqlite3_errmsg(db)" detail of errors
+        cerr<<"there is a error to open database -> det: "<<sqlite3_errmsg(db)<<endl;
+
+    } else
+        cout<<"databace succesfuly "<<endl;
+
+    sql_query = "CREATE TABLE IF NOT EXISTS users "
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                "name TEXT NOT NULL,"
+                "username TEXT NOT NULL,"
+                "how_they_find_us TEXT NOT NULL,"
+                "timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);";
+
+    return_code = sqlite3_exec(db,sql_query.c_str(),NULL,0,&errmsg);
+
+    if (return_code != SQLITE_OK)
+    {
+        //this option for sqlite "sqlite3_errmsg(db)" detail of errors
+        cerr<<"error for creat table -> det: "<<errmsg<<endl;
+        sqlite3_free(errmsg);
+    } else
+        cout<<"table  succesfuly create"<<endl;
+
+
+    string name = "sadra" , username = "sadraq" , how_they_find_us = "so hard";
+    insert_data(db,name,username,how_they_find_us);
+
+
+
+    sqlite3_close(db);
 
 }
+int callback(void* data, int argc, char** argv, char** colNames)
+{
+    for (int i = 0; i < argc; i++) {
+        cout << colNames[i] << ": " << (argv[i] ? argv[i] : "NULL") << " | ";
+    }
+    cout << endl;
+    return 0;
+}
+
+int insert_data(sqlite3 *db ,string name,string username,string how_they_find_us)
+{
+    stringstream query;
+    query<<"INSERT INTO users(name,username,how_they_find_us) VALUES('"<<name<<
+    "' , '"<<username<<"' , '" <<how_they_find_us<<"');";
+
+    int rc;
+    char* errormessage = 0;
+    string finalquery;
+
+    finalquery = query.str();
+
+    rc = sqlite3_exec(db,finalquery.c_str(),callback,0,&errormessage);
+    if (rc != SQLITE_OK)
+    {
+        //this option for sqlite "sqlite3_errmsg(db)" detail of errors
+        cerr<<"error for add data -> det: "<<errormessage<<endl;
+        sqlite3_free(errormessage);
+    } else
+        cout<<"data  succesfuly add"<<endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
